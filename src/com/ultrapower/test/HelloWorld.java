4bc0bde23133c684f8ae8de52028508185101b07
zyjDarkness <992517931@qq.com>
2019-04-15 19:59:01 +0800

    commit提交到本地0415

--- src/com/ultrapower/test/HelloWorld.java
+++ src/com/ultrapower/test/HelloWorld.java
@@ -3,6 +3,8 @@ package com.ultrapower.test;
 import com.ultrapower.bean.User;
 import sun.applet.Main;
 
+import java.util.ArrayList;
+
 /**
  * 快捷键的使用
  */
@@ -11,7 +13,20 @@ public class HelloWorld {
     private static final User USER = new User();
 
     public static final int A = 1;
+    /**
+     * 测试
+     */
+    private int a = 1;
+
     public static void main(String[] args) {
-        System.out.println();
+
+
     }
+    
+    public void testDebug(String  name){
+        System.out.println("debug测试~");
+        System.out.println("name = [" + name + "]");
+
+    }
+
 }
